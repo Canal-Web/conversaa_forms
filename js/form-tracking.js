67df648a6b349dc957d636421c29ddd7e077e814
Fwagra <fabiocerqueiracw@gmail.com>
2016-02-23 14:58:42 +0100

    Reshape the form configuration

--- js/form-tracking.js
+++ js/form-tracking.js
@@ -1,15 +1,18 @@
 jQuery(document).ready(function($) {
 	var forms = Drupal.settings.conversaa_form.forms;
-	$('form').on('submit', function(event) {
-		var formId = $(this).attr('id');
-		if(forms.hasOwnProperty(formId)){
-			var tracking = $.conversaaPixel();
-			var values = '';
-			$.each(forms[formId]['fields'], function(index, val) {
-				values += '&'+index+'='+encodeURIComponent($("[name="+val+"]").val());
-			});
-			tracking += values;
-			$('body').append('<img src="'+tracking+'" />');
-		}
-	});
+	if(forms){
+		$('form').on('submit', function(event) {
+			var formId = $(this).attr('id');
+			if(forms.hasOwnProperty(formId)){
+			console.log('yes');
+				var tracking = $.conversaaPixel();
+				var values = '';
+				$.each(forms[formId]['fields'], function(index, val) {
+					values += '&'+index+'='+encodeURIComponent($("[name='"+val+"']").val());
+				});
+				tracking += values;
+				$('body').append('<img src="'+tracking+'" />');
+			}
+		});
+	}
 });
